81ad48a6b2413993686da10ff29b67f7b393b407
Maxim Gabidullin <after@ya.ru>
2022-04-09 00:06:58 +0300

    delete monolog from composer.json

---
+++ app/controllers/docs/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\controllers\docs;
+
+use easy\basic\router\Route;
+use easy\MVC\Controller;
+
+class IndexController extends Controller
+{
+    #[Route('/docs', name: 'docs_index')]
+    public function index()
+    {
+        $this->render('docs/index');
+    }
+}
