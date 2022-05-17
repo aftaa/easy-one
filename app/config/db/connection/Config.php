01b9365dbfee752c25a969da31ae40f3461a38ff
Maxim Gabidullin <after@ya.ru>
2022-05-18 00:52:33 +0300

    add MYSQL_HOSTNAME for correct work with Docker

--- app/config/db/connection/Config.php
+++ app/config/db/connection/Config.php
@@ -4,9 +4,15 @@ namespace app\config\db\connection;
 
 class Config
 {
-    public string $provider = "mysql";
+    public string $provider = 'mysql';
     public string $hostname = 'localhost';
     public string $username = 'symfony';
     public string $password = 'symfony';
     public string $database = 'symfony';
+
+    public function __construct()
+    {
+        $mysqlHostname = $_ENV['MYSQL_HOSTNAME'] ?? 'localhost';
+        $this->hostname = $mysqlHostname;
+    }
 }
